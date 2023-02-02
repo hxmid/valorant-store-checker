a6362a458e4acbcf7fc1e6e5e1e0bece7d9f6d04
hamid <hamid.haxhimolla@gmail.com>
2023-02-02 18:29:48 +1100

    moved some things around. refactoring inbound

--- stores.py
+++ stores.py
@@ -1,4 +1,5 @@
 from riot_auth import RiotAuth
+from riot_auth.auth_exceptions import RiotAuthenticationError
 import asyncio
 import requests
 from termcolor import colored
@@ -8,57 +9,54 @@ watchlist_tiers = {
         "colour": "red",
         "skins": [
             "Xenohunter Knife",
-            "Protocol 781-A Sheriff",
             "VALORANT GO! Vol. 1 Knife",
+            "Protocol 781-A Sheriff",
+            "Chronovoid Vandal",
         ],
         "value": 5
     },
     "a": {
         "colour": "yellow",
         "skins": [
-            "Protocol 781-A Phantom",
-            "Sovereign Sword",
-            "RGX 11z Pro Vandal",
-            "Sentinels of Light Sheriff",
         ],
         "value": 4
     },
     "b": {
         "colour": "green",
         "skins": [
-            "Forsaken Vandal",
-            "Glitchpop Vandal",
-            "Gaia's Vengeance Vandal",
-            "Sentinels of Light Vandal"
+            "Araxys Vandal",
+            "Equilibrium",
         ],
         "value": 3
     },
     "c": {
         "colour": "cyan",
         "skins": [
-            "Ruination Phantom",
-            "Catrina",
-            "Magepunk Sheriff",
-            "Singularity Phantom",
+            "Sovereign Sword",
+            "Forsaken Vandal",
+            "RGX 11z Pro Vandal",
+            "Sovereign Ghost",
+            "Chronovoid Sheriff",
+            "Protocol 781-A Phantom",
+            "Luna's Descent",
         ],
         "value": 2
     },
     "d": {
         "colour": "blue",
         "skins": [
-            "Araxys Vandal",
-            "Ion Karambit",
-            "Prime Guardian"
+            "Glitchpop Vandal",
+            "Gaia's Vengeance Vandal",
+            "Sentinels of Light Vandal",
+            "Sentinels of Light Sheriff",
+            "Ruination Phantom",
         ],
         "value": 1
     },
     "e": {
         "colour": "magenta",
         "skins": [
-            "Araxys Bio Harvester",
-            "Oni Phantom",
-            "Blade of Chaos",
-            "Oni Claw",
+            "Catrina",
         ],
         "value": 0
     }
@@ -67,6 +65,7 @@ watchlist_tiers = {
 
 class skin:
     VP_INFO = requests.get("https://valorant-api.com/v1/currencies").json()["data"][0]
+    EQUIPS = tuple(map(lambda x: x["name"], requests.get("https://api.henrikdev.xyz/valorant/v1/content").json()["equips"]))
 
     def __init__(self, store_item: dict):
         self.cost = store_item["Cost"][skin.VP_INFO["uuid"]]
@@ -78,26 +77,26 @@ class skin:
             print(f"error [{skin_resp.status_code}]: could not get skin")
             exit()
 
-        self.name = skin_resp.json()["data"]["displayName"]
+        self.name: str = skin_resp.json()["data"]["displayName"]
 
         for tier in watchlist_tiers:
             if self.name in watchlist_tiers[tier]["skins"]:
                 self.colour = watchlist_tiers[tier]["colour"]
                 self.value = pow(2, watchlist_tiers[tier]["value"])
-
                 break
         else:
-            self.colour = "grey"
+            self.colour = "grey" if self.name.endswith(skin.EQUIPS) else "white"
             self.value = 0
 
     def __str__(self):
         return colored(f"<{self.cost} VP> {self.name}", self.colour)
 
+
 class account:
     def __init__(self, credentials: str):
         (self.region, self.u, self.p) = credentials.split(":", maxsplit=2)
         self.name: str = ""
-        self.tag:str = ""
+        self.tag: str = ""
         self.store: list(skin) = []
         self.score = 0
 
@@ -112,8 +111,10 @@ class account:
     def __str__(self):
         if self.name == None:
             self.name = ""
+
         if self.tag == None:
             self.tag = ""
+
         return  f"{self.u + ':' : <25} {self.name : >16} #{self.tag : <5} ({self.score : >2}) -> " + "[ " + ", ".join([str(x) for x in self.store]) + " ]"
 
     def print(self, i):
@@ -122,7 +123,12 @@ class account:
     def write(self, i):
         return self.print(i) + "\n"
 
-def get_store(auth: RiotAuth, acc: account) -> account:
+
+def get_store(acc: account) -> account:
+    client_version: str = requests.get("https://valorant-api.com/v1/version").json()["data"]["riotClientBuild"]
+    RiotAuth.RIOT_CLIENT_USER_AGENT = f"RiotClient/{client_version} %s (Windows;10;;Professional, x64)"
+    asyncio.set_event_loop(asyncio.new_event_loop())
+    auth = RiotAuth()
     asyncio.run(auth.authorize(acc.u, acc.p))
 
     userinfo = requests.get("https://auth.riotgames.com/userinfo", headers={"Authorization": f"{auth.token_type} {auth.access_token}"})
@@ -145,7 +151,7 @@ def get_store(auth: RiotAuth, acc: account) -> account:
     return acc
 
 
-def main():
+def main() -> None:
     while True:
         response = input("[g]enerate new or [d]ump previous ? ")
 
@@ -159,13 +165,13 @@ def main():
 
         print(f"'{response}' wasn't an option. try again")
 
-def dump():
-    print(open("dump.txt", 'r', encoding= "utf-8").read())
 
-def generate():
-    client_version = {requests.get("https://valorant-api.com/v1/version").json()["data"]["riotClientBuild"]}
-    RiotAuth.RIOT_CLIENT_USER_AGENT = f"RiotClient/{client_version} %s (Windows;10;;Professional, x64)"
-    auth = RiotAuth()
+def dump() -> None:
+    print(open("dump.txt", 'r', encoding = "utf-8").read())
+
+
+def generate() -> None:
+
 
     with open("accounts.txt", 'r') as f:
         with open("dump.txt", 'w', encoding = "utf-8") as d:
@@ -174,8 +180,12 @@ def generate():
             for i, details in enumerate(f.read().splitlines()):
                 if details == "---":
                     break
-
-                accs.append(get_store(auth, account(details)))
+                acc = account(details)
+                try:
+                    accs.append(get_store(acc))
+                except RiotAuthenticationError:
+                    print(f"error: invalid user/pass for account \'{acc.u}\'")
+                    continue
 
                 print(f"  parsed {i + 1} account{'s' if i != 0 else ''}... please wait :)", end = '\r')
 
@@ -184,7 +194,6 @@ def generate():
             for i, acc in enumerate(accs):
                 print(acc.print(i))
                 d.write(acc.write(i))
-
 
 
 if __name__ == "__main__":
